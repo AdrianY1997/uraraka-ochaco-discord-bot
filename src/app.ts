81d5892bf47bae6fc61eb8bc93c815a1aaefd502
HappyFox.Devs <yasnoadriandatos@gmail.com>
2023-04-13 13:13:59 -0500

    First Commit

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
+import * as http from "http"
 import dotenv from "dotenv";
 dotenv.config()
 
 import { CustomClient } from "./Component/CustomClient";
 import { BOT_TOKEN } from "./Config/Bot";
+
+http.createServer((req, res) => {
+    res.write("I'm alive");
+    res.end();
+}).listen(8080)
 
 const client = new CustomClient({
     intents: 3276799
